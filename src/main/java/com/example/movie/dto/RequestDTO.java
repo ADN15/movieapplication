5bc24fc998a44ab00c1f39e83d2c185517d181fe
10991981 <v.muhamad.rifai@adira.co.id>
2021-07-09 08:16:49 +0700

    all

---
+++ src/main/java/com/example/movie/dto/RequestDTO.java
@@ -0,0 +1,30 @@
+package com.example.movie.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class RequestDTO {
+	
+	@Getter
+	@Setter
+	@JsonProperty("movie_id")
+	private String movieId;
+	
+	@Getter
+	@Setter
+	@JsonProperty("search")
+	private String search;
+	
+	@Getter
+	@Setter
+	@JsonProperty("title")
+	private String title;
+	
+	@Getter
+	@Setter
+	@JsonProperty("year")
+	private String year;
+	
+}
